1394deff6f630d082828b5289876ba1080d4bc2c
Abhik Khanra <abhik.rk@gmail.com>
2016-07-12 23:33:38 +0530

    Dynamically displaying Descriptions and Simplified description based on selected section

--- controllers/index.server.controller.js
+++ controllers/index.server.controller.js
@@ -8,70 +8,87 @@ exports.render = function(req, res) {
 	        				"section" : {
 		        				"article" : "Extent",
 		        				"description" : "This Act extends in the first instance to the territories which, immediately before the 1st November,  were comprised in the States of Bombay, Saurashtra and Kutch, but the Central Government may, by notification in the Official Gazette, extend it to any other State or part of a State which has a sea-coast.",
-		        				"simplified" : "In which we try to explain formidable legalese using everyday words."
+		        				"simplified" : "In which we try to explain formidable legalese using everyday words for Article 1."
 	        				}
 	        			}, 
 	                    {
 	                    	"section" : {
 		                     	"article" : "Rules as to coasting and other vessels belonging to any citizen of India",
-		                     	"description" : {}
+		                     	"description" : "The following rules shall be in force with respect to vessels belonging to any citizen of India and employed on the coasts of any State or part of a State to which this Act extends or in trading coastwise, as also with respect to fishing- vessels and harbour- craft belonging to any such citizen.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 2."
 	                    	}
 	                    }, 
 	                    {
 	                    	"section" : {
 		                     	"article" : "Marking or branding vessels with name of place and number",
-		                     	"description" : {}
+		                     	"description" : "Every such vessel employed as aforesaid, fishing- vessel and harbour- craft shall be marked or branded with the name of the place to which she belongs, and also with a number assigned for the same by the officer authorised to make such registry as is hereinafter mentioned; Owner to paint name and number-- and the owner or owners of such vessel employed as aforesaid, fishing- vessel and harbour- craft shall cause such name and number to be painted in black paint upon a white ground on each quarter of such vessel employed as aforesaid, fishing- vessel and harbour- craft, in English figures and letters, each figure and letter being six inches in length.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 3."
 	                    	}
 	                    },
 	                    {	"section" : { 
 		                     	"article" : "Registry of name, number and burthen",
-		                     	"description" : {}
+		                     	"description" : "The name and number of every such vessels employed as aforesaid, fishing- vessel and harbour- craft, and her burthen, and also the name or names of the  owner or owners thereof, shall be registered in a book to be kept for that purpose by the person hereinafter directed to make such registry.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 4."
 		                     }
-	                    },
-	                    {
-	                    	"section" : {
-		                     	"article" : "Marking or branding vessels with name of place and number",
-		                     	"description" : {}
-	                    	}
 	                    },
 	                    {	
 	                    	"section" : {
 		                     	"article" : "Owners to apply for registry",
-		                     	"description" : {}
+		                     	"description" : "The owner or owners of every such vessel employed as aforesaid, fishing- vessel and harbour-craft shall apply to the person authorised to make such registry in respect of the same, in order to have such registry as aforesaid made, or in order to have such registry made again as aforesaid. Information of registry at subordinate port.-- And whenever such vessel employed as aforesaid, fishing- vessel or harbour- craft is registered at a subordinate port, information thereof, and of the number there assigned to her, shall immediately be given by the registering officer to the Principal Officer, Mercantile Marine Department at Bombay.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 5."
 	                    	}
 	                    },
 	                    {
 	                    	"section" : {
 		                     	"article" : "Officers to perform duty of marking and branding",
-		                     	"description" : {}
+		                     	"description" : "The duty of marking or branding and of ascertaining the burthen of such vessels employed as aforesaid, fishing- vessels and harbour- craft, at Bombay, shall be performed by the Principal Officer, Mercantile Marine Department]; and at all other places the duty of marking or branding and of ascertaining the burthen of such vessels employed as aforesaid, fishing- vessels and harbour- craft shall be performed by the Collector of Sea- customs at such places respectively, or by such other persons as shall be appointed by the Central Government to act at such places respectively, in the execution of this Act.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 6."
 	                    	}
 	                    },
 	                    {
 	                    	"section" : {
-		      	             	"article" : "Sealing certificate",
-		      	             	"description" : {}
+		      	             	"article" : "Owner to obtain certificate of registry Replacing lost certificate",
+		      	             	"description" : "The owner or owners of every such vessel employed as aforesaid, fishing- vessel and harbour- craft shall apply for and obtain a certificate of registry from the person authorised to make such registry as aforesaid, and such certificate shall be in the form specified in the Schedule appended to this Act; and in the case of any certificate being lost or destroyed, a renewed certificate may be obtained in the same manner and on payment of the fees hereinafter mentioned.",
+		      	             	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 7."
 		      	            }
 	                    },
 	                    {
 	                    	"section" : {
-	                     	"article" : "Fees for certificate",
-	                     	"description" : {}
+		      	             	"article" : "Sealing certificate",
+		      	             	"description" : "Such certificate of registry shall be sealed with the seal of the Government of India, and shall be signed by the person authorised to make such registry.",
+		      	             	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 8."
+		      	            }
+	                    },
+	                    {
+	                    	"section" : {
+		                     	"article" : "Fees for certificate",
+		                     	"description" : "The owner or owners of such vessels employed as aforesaid (fishing- vessels and harbour- craft being excepted) on being registered as aforesaid, shall pay for each certificate of registry for a vessel not exceeding 5 tons burthen, the fee of... 1 rupee; for each certificate for a vessel exceeding 5 tons burthen and not exceeding 25 tons burthen, the fee of.... 5 rupees.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 9."
 	                    	}
 	                    },
 	                    {
 	                    	"section" : {
 		                     	"article" : "Fees to be credited to Government",
-		                     	"description" : {}
+		                     	"description" : "The person or persons so authorised to make such registry as aforesaid shall receive the fees payable for the same, and shall pay such fees to such officer as the Central Government shall appoint; the same to be carried to the credit of the Central Government provided that any such fees as immediately before the commencement of the Constitution were, under this Act as then in force to be carried to the credit of the Provincial Government shall be paid to such officer as the State Government may appoint and be carried to the credit of that Government.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 10.."
 	                    	}
 	                    },
 	                    {	"section" : {
 		                    	"article" : "Production of certificate on demand",
-		                    	"description" : {}
+		                    	"description" : "The owner or owners or commander of every such Vessel employed as aforesaid, fishing- vessel and harbour- craft shall produce, on demand thereof by any officer of the Customs or by any officer of the Navy, the certificate so directed to be applied for and obtained in respect of such vessel employed as aforesaid, fishing- vessel or harbour- craft, as above mentioned.",
+		                    	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 11."
 	                    	}
 	                    },
 	                    {	"section" : {
 		                     	"article" : "Penalty for neglect to comply with rules",
-		                     	"description" : {}
+		                     	"description" : "In case any such vessel employed as aforesaid, fishing- vessel or harbour- craft shall not be so marked or branded in all respects as hereinbefore directed, or in case the name and number of any such vessel employed as aforesaid, fishing- vessel or harbour- craft shall not be so painted, or shall not continue so painted on such vessel employed as aforesaid, fishing- vessel or harbour- craft, in all respects as hereinbefore directed; or in case any such vessel employed as aforesaid, fishing- vessel or harbour- craft shall not be furnished with such certificate as hereinbefore specified, or in case the owner or owners or commander of any such vessel employed as aforesaid, fishing- vessel or harbour- craft shall not produce such certificate on demand thereof as hereinbefore directed; the owner or owners of every such vessel employed as aforesaid shall be subject to a fine of ten times the amount of the fees payable in respect of the certificate of registry of such vessel, the same being a vessel for the certificate of the registration of which any fee is payable; and the owner or owners of any such fishing- vessel or harbour- craft shall be subject to a fine of ten rupees; Recovery of penalties-- which fines may be recovered on conviction before any Magistrate  Having jurisdiction, by sale of such vessel, fishing- vessel or harbour- craft, her furniture, ammunition, tackle and apparel; Penalty on repetition of default-- and such fines shall be payable as often as the owner or owners or commander of any such vessel employed as aforesaid, fishing- vessel or harbour- craft shall make such default as aforesaid: Provided every such subsequent default be made after the expiration of one month from the date of the last conviction.",
+		                     	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 12."
+	                    	}
+	                    },
+	                    {	"section" : {
+		                    	"article" : "Power to direct compensation for trouble in seizing",
+		                    	"description" : "Central Government] may direct compensation for trouble and diligence in seizing such vessel employed as aforesaid, fishing- vessel or harbour- craft, guns, furniture, tackle, ammunition and apparel, as last mentioned, to be made, out of the proceeds of such seizure to the person or persons who shall have seized the same, to such amount, in such manner and in such shares or proportions, as to the said Central Government] shall seem meet.",
+		                    	"simplified" : "In which we try to explain formidable legalese using everyday words for Article 13."
 	                    	}
 	                    }
                     ]
